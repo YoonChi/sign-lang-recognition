b0d90b8423185a9a747092d06dae87df0d084003
Yoon Chi <40680312+YoonChi@users.noreply.github.com>
2022-04-25 11:08:29 -0500

    Update convert-to-grayscale.py

--- ASL_Code_New/convert-to-grayscale.py
+++ ASL_Code_New/convert-to-grayscale.py
@@ -6,17 +6,20 @@ import numpy as np
 import tensorflow as tf
 import os 
 import tensorflow_io as tfio
-from PIL import Image
+from PIL import Image 
 
-path = '/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/gesture/asl_alphabet_train/A'
+path = r'/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/gesture/asl_alphabet_train/A'
 
 files = os.listdir(path)
 
 for index, file in enumerate(files):
-    img = Image.open(os.path.join(path, file)) # open file
+    # Your best bet is to attempt to open im = Image.open() and validate by calling im.load() all of your images, and record the filenames that fail.
+    joined_path = os.path.join(path, file)
         
-    image = tf.image.decode_jpeg(tf.io.read_file(img)) # read as jpeg file
-    print(image.shape, image.dtype) # validate
+    img = Image.open(joined_path)
+    
+    image = tf.image.decode_jpeg(tf.io.read_file(img))
+    print(image.shape, image.dtype)
 
     grayscale = tfio.experimental.color.rgb_to_grayscale(image)
     
