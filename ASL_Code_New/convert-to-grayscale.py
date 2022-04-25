7d086826966d4784efe89f7281429a0a8d7ae425
Yoon Chi <40680312+YoonChi@users.noreply.github.com>
2022-04-25 10:45:09 -0500

    Create convert-to-grayscale.py
    iterates through directory and converts rgb image to grayscale

---
+++ ASL_Code_New/convert-to-grayscale.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import os 
+import tensorflow_io as tfio
+from PIL import Image
+
+path = '/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/gesture/asl_alphabet_train/A'
+
+files = os.listdir(path)
+
+for index, file in enumerate(files):
+    img = Image.open(os.path.join(path, file))
+        
+    image = tf.image.decode_jpeg(tf.io.read_file(img))
+    print(image.shape, image.dtype)
+
+    grayscale = tfio.experimental.color.rgb_to_grayscale(image)
+    
+    print(grayscale.shape, grayscale.dtype)
+    # use tf.squeeze to remove last channel for plt.imshow to display:
+    plt.figure()
+    plt.imshow(tf.squeeze(grayscale, axis=-1), cmap='gray')
+    plt.axis('off')
+    plt.show()
+    
+    break
