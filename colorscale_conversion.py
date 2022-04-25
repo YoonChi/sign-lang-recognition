3fa64005957c1069541bc30d751c4056d063d0cd
Yoon Chi <40680312+YoonChi@users.noreply.github.com>
2022-04-25 16:34:34 -0500

    Update colorscale_conversion.py

--- colorscale_conversion.py
+++ colorscale_conversion.py
@@ -1,6 +1,11 @@
-from matplotlib import pyplot as plt
-import matplotlib.image as mpimg
-import cv2
+# path = r'/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/gesture/asl_alphabet_train/A/A26.jpg'
+
+# img = Image.open(path).convert('L')
+# img.save('greyscale_A1.jpg')
+# inv_img = ImageChops.invert(img)
+# inv_img.save('inverted_grayscale_A26.jpg')
+
+
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -19,19 +24,16 @@ for index, folder in enumerate(folders):
     print(folder)
     if folder != ".DS_Store":
         joined_path = os.path.join(path, folder)
-
+        # print(joined_path)
+        
         files = os.listdir(joined_path)
         for index, file in enumerate(files):
+            # print(os.path.join(joined_path,file))
             img = Image.open(os.path.join(joined_path,file)).convert('L')
-            # img.save('greyscale_'+file+'.jpg')
             inv_img = ImageChops.invert(img)
-            inv_img.save('inverted_grayscale_'+file+'.jpg')
-            imgname  = 'inverted_grayscale_'+file+'.jpg'
+            inv_img.save('inverted_grayscale_'+file)
+            # imgname  = 'inverted_grayscale_'+file'
             
-            source = '/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/code/'+imgname
-            
-            dest = path + '/' +folder # destination path 
-            
-            shutil.move(source, dest)
-
-            break
+            src_path = '/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/code/'+'inverted_grayscale_'+file
+            dest_path = '/Users/yooniiechi/Desktop/AI/AI_PROJECT-ASL/gestures/inverted_kaggle_set/'+folder            
+            shutil.move(src_path, dest)
